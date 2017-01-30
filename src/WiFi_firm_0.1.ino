97e33c8231d3c6f5d3ef3c9dcbfebf984f7bf15c
JuanMiguel <juanmiguel@Synergy.local>
2017-01-29 21:51:20 -0300

    funcion WiFi.isConnected

--- src/WiFi_firm_0.1.ino
+++ src/WiFi_firm_0.1.ino
@@ -8,7 +8,8 @@
 
 char server[] = "www.indoormatic.com.ar";
 
-String inputString      = "";// a string to hold incoming data
+String GET      = "";// a string to hold incoming data
+String inputString = "";
 boolean stringComplete  = false;  // whether the string is complete
 boolean datoRecibido    = false;  // cuando es verdadero lee el dato de serie
 // Inicializa la libreria Wifi client
@@ -17,56 +18,77 @@ WiFiClient client;
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
+    //WiFiManager - Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
-    //reset saved settings
-    //wifiManager.resetSettings();
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
+    //wifiManager.resetSettings(); //reset saved settings
     wifiManager.autoConnect("IndoorMatic");
     //if you get here you have connected to the WiFi
-    Serial.println("CONECTADO");
+    Serial.println("CONECTADO_OK");
 }
 
 void loop()
 {
-  inputString      = "";// a string to hold incoming data
-
-  while (Serial.available() > 0)
-  {
-    //Serial.print(Serial.readString());
-    inputString = Serial.readString();
-    datoRecibido = true;
-  }
+  inputString = escuchaSerial();
+  GET = analizaComando(inputString);
 
   if (datoRecibido)
   {
+
     datoRecibido = false;
-    String GET = "GET http://www.indoormatic.com.ar/test.php?debug=";
-    GET += inputString;
-    Serial.println(inputString);
+
+    Serial.println( WiFi.isConnected() ) ;
 
     if (client.connect(server, 80))
     {
-      Serial.println("connecting...");
+      //Serial.println("connecting...");
       // envia el request http
-     client.println(GET);
-     // cierra la conexion
-     client.println("Connection: close");
-     // imprime el request http
-     Serial.println(GET);
-     // imprime la respuesta del servidor
-     while (client.available())
-     {
-       Serial.write(client.read());
-     }
+      client.println(GET);
+      // cierra la conexion
+      client.println("Connection: close");
+      // imprime el request http
+      //Serial.println(GET);
+      // imprime la respuesta del servidor
+      while (client.available())
+      {
+        Serial.write(client.read());
+      }
     }
     else
     {
     // if you couldn't make a connection:
     Serial.println("connection failed");
     }
+  }
+}
+
+
+// Escucha el puerto serie y si hay dato lo devuelve en un string.
+String escuchaSerial()
+{
+  String stringSerial = "";// a string to hold incoming data
+  while (Serial.available() > 0)
+  {
+    //Serial.print(Serial.readString());
+    stringSerial = Serial.readString();
+  }
+  return stringSerial;
+}
+
+//parsea un string para ver que tipo de comando es
+String analizaComando(String comando)
+{
+  String comandoAnalizado = "";
+  //comando.trim();
+  if( comando.startsWith( "<" ) && comando.endsWith( ">" ) )
+  {
+    datoRecibido = true;
+    comando = comando.substring( 1, comando.length() - 1) ;
+    comandoAnalizado = "GET http://www.indoormatic.com.ar/test.php?debug=";
+    comandoAnalizado += comando;
+    return comandoAnalizado;
+  }
+  else
+  {
+    datoRecibido = false;
   }
 }
